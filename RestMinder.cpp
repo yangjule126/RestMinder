8f7b67787966a8beb560859fd40f5f2b580b72f6
yangjule126 <42511667+yangjule126@users.noreply.github.com>
2026-03-09 10:39:15 +0800

    Update RestMinder.cpp

--- RestMinder.cpp
+++ RestMinder.cpp
@@ -3,6 +3,8 @@
 #include <string>
 
 #pragma comment(lib, "user32.lib")
+#include <shlwapi.h>
+#pragma comment(lib, "shlwapi.lib")
 
 #define WM_TRAYICON (WM_USER + 1)
 
@@ -14,7 +16,8 @@
 #define ID_BTN_SAVE      2001
 #define ID_BTN_CLOSE     2002
 
-#define APP_VERSION      L"v1.1.0"
+
+#define APP_VERSION      L"v1.2.0"
 
 NOTIFYICONDATA nid = { 0 };
 UINT WM_TASKBARCREATED;   // ? 新增
@@ -29,6 +32,9 @@ ULONGLONG workingTime = 0;
 
 HWND g_hSettings = NULL;
 HWND hEditWork, hEditAway, hEditCheck;
+HWND hCheckAutoStart;
+HWND g_hGithub = NULL;
+HFONT g_hLinkFont = NULL;
 
 // ================= 获取EXE目录 =================
 std::wstring GetExeDirectory()
@@ -61,6 +67,68 @@ void ReloadConfig()
     CHECK_INTERVAL = (ULONGLONG)checkSec * 1000;
 }
 
+// ================= 获取程序路径 =================
+std::wstring GetExePath()
+{
+    wchar_t path[MAX_PATH];
+    GetModuleFileName(NULL, path, MAX_PATH);
+    return path;
+}
+
+// ================= 是否开机启动 =================
+bool IsAutoStartEnabled()
+{
+    HKEY hKey;
+    if (RegOpenKey(HKEY_CURRENT_USER,
+        L"Software\\Microsoft\\Windows\\CurrentVersion\\Run",
+        &hKey) != ERROR_SUCCESS)
+        return false;
+
+    wchar_t value[MAX_PATH];
+    DWORD size = sizeof(value);
+
+    bool enabled = (RegQueryValueEx(
+        hKey,
+        L"SedentaryReminder",
+        NULL,
+        NULL,
+        (LPBYTE)value,
+        &size) == ERROR_SUCCESS);
+
+    RegCloseKey(hKey);
+    return enabled;
+}
+
+// ================= 设置开机启动 =================
+void SetAutoStart(bool enable)
+{
+    HKEY hKey;
+
+    if (RegOpenKey(HKEY_CURRENT_USER,
+        L"Software\\Microsoft\\Windows\\CurrentVersion\\Run",
+        &hKey) != ERROR_SUCCESS)
+        return;
+
+    if (enable)
+    {
+        std::wstring path = GetExePath();
+
+        RegSetValueEx(
+            hKey,
+            L"SedentaryReminder",
+            0,
+            REG_SZ,
+            (BYTE*)path.c_str(),
+            (DWORD)((path.size() + 1) * sizeof(wchar_t)));
+    }
+    else
+    {
+        RegDeleteValue(hKey, L"SedentaryReminder");
+    }
+
+    RegCloseKey(hKey);
+}
+
 // ================= 获取系统空闲时间 =================
 ULONGLONG GetIdleTime()
 {
@@ -85,15 +153,19 @@ void ShowReminder()
 // ================= 关于 =================
 void ShowAbout()
 {
-    std::wstring msg =
-        L"久坐提醒程序\n\n"
-        L"作者：基础运维部\n"
-        L"版本号：" APP_VERSION;
-
-    MessageBox(NULL,
-        msg.c_str(),
+    HWND hWnd = CreateWindowEx(
+        WS_EX_TOPMOST,
+        L"AboutWindow",
         L"关于",
-        MB_OK | MB_ICONINFORMATION | MB_TOPMOST);
+        WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU,
+        600, 300, 260, 200,
+        NULL,
+        NULL,
+        GetModuleHandle(NULL),
+        NULL);
+
+    ShowWindow(hWnd, SW_SHOW);
+    UpdateWindow(hWnd);
 }
 
 // ================= 托盘 =================
@@ -122,44 +194,57 @@ LRESULT CALLBACK SettingsProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
     {
     case WM_CREATE:
     {
-        CreateWindow(L"STATIC", L"工作提醒(分钟):",
+        CreateWindow(L"STATIC", L"连续使用电脑多久后提醒(分钟):",
             WS_VISIBLE | WS_CHILD,
-            20, 20, 120, 20,
+            20, 20, 220, 20,
             hWnd, NULL, NULL, NULL);
 
         hEditWork = CreateWindow(L"EDIT", L"",
             WS_VISIBLE | WS_CHILD | WS_BORDER | ES_NUMBER,
-            150, 20, 100, 20,
+            250, 20, 100, 20,
             hWnd, NULL, NULL, NULL);
 
-        CreateWindow(L"STATIC", L"离开重置(分钟):",
+        CreateWindow(L"STATIC", L"未使用电脑多久重置计数(分钟):",
             WS_VISIBLE | WS_CHILD,
-            20, 60, 120, 20,
+            20, 60, 220, 20,
             hWnd, NULL, NULL, NULL);
 
         hEditAway = CreateWindow(L"EDIT", L"",
             WS_VISIBLE | WS_CHILD | WS_BORDER | ES_NUMBER,
-            150, 60, 100, 20,
+            250, 60, 100, 20,
             hWnd, NULL, NULL, NULL);
 
-        CreateWindow(L"STATIC", L"检测间隔(秒):",
+        CreateWindow(L"STATIC", L"自动检测间隔(秒):",
             WS_VISIBLE | WS_CHILD,
-            20, 100, 120, 20,
+            20, 100, 150, 20,
             hWnd, NULL, NULL, NULL);
 
         hEditCheck = CreateWindow(L"EDIT", L"",
             WS_VISIBLE | WS_CHILD | WS_BORDER | ES_NUMBER,
-            150, 100, 100, 20,
+            180, 100, 100, 20,
             hWnd, NULL, NULL, NULL);
+
+        hCheckAutoStart = CreateWindow(
+            L"BUTTON",
+            L"开机自启",
+            WS_VISIBLE | WS_CHILD | BS_AUTOCHECKBOX,
+            20, 140, 120, 20,
+            hWnd,
+            NULL,
+            NULL,
+            NULL);
+
+        if (IsAutoStartEnabled())
+            SendMessage(hCheckAutoStart, BM_SETCHECK, BST_CHECKED, 0);
 
         CreateWindow(L"BUTTON", L"保存",
             WS_VISIBLE | WS_CHILD,
-            50, 150, 80, 30,
+            50, 180, 80, 30,
             hWnd, (HMENU)ID_BTN_SAVE, NULL, NULL);
 
         CreateWindow(L"BUTTON", L"关闭",
             WS_VISIBLE | WS_CHILD,
-            150, 150, 80, 30,
+            150, 180, 80, 30,
             hWnd, (HMENU)ID_BTN_CLOSE, NULL, NULL);
 
         wchar_t buf[32];
@@ -193,6 +278,12 @@ LRESULT CALLBACK SettingsProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
             WritePrivateProfileString(L"Settings", L"CheckIntervalSeconds", buf, g_configPath.c_str());
 
             ReloadConfig();
+
+            bool autoStart =
+                SendMessage(hCheckAutoStart, BM_GETCHECK, 0, 0) == BST_CHECKED;
+
+            SetAutoStart(autoStart);
+
             MessageBox(hWnd, L"保存成功", L"提示", MB_OK);
             break;
         }
@@ -205,6 +296,138 @@ LRESULT CALLBACK SettingsProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
     case WM_DESTROY:
         g_hSettings = NULL;
         return 0;
+    }
+
+    return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
+LRESULT CALLBACK AboutProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch (msg)
+    {
+    case WM_CREATE:
+    {
+        HFONT hTitleFont = CreateFont(
+            26, 0, 0, 0, FW_BOLD,
+            FALSE, FALSE, FALSE,
+            DEFAULT_CHARSET,
+            OUT_DEFAULT_PRECIS,
+            CLIP_DEFAULT_PRECIS,
+            CLEARTYPE_QUALITY,
+            DEFAULT_PITCH | FF_SWISS,
+            L"Microsoft YaHei");
+
+        HFONT hFont = (HFONT)GetStockObject(DEFAULT_GUI_FONT);
+
+        HWND hTitle = CreateWindow(
+            L"STATIC",
+            L"久坐提醒",
+            WS_VISIBLE | WS_CHILD,
+            20, 20, 200, 30,
+            hWnd,
+            NULL, NULL, NULL);
+
+        SendMessage(hTitle, WM_SETFONT, (WPARAM)hTitleFont, TRUE);
+
+        std::wstring version = L"版本 ";
+        version += APP_VERSION;
+
+        HWND hVersion = CreateWindow(
+            L"STATIC",
+            version.c_str(),
+            WS_VISIBLE | WS_CHILD,
+            20, 70, 200, 20,
+            hWnd,
+            NULL, NULL, NULL);
+
+        SendMessage(hVersion, WM_SETFONT, (WPARAM)hFont, TRUE);
+
+        HWND hAuthor = CreateWindow(
+            L"STATIC",
+            L"作者：基础运维部",
+            WS_VISIBLE | WS_CHILD,
+            20, 95, 200, 20,
+            hWnd,
+            NULL, NULL, NULL);
+
+        SendMessage(hAuthor, WM_SETFONT, (WPARAM)hFont, TRUE);
+
+        // GitHub 链接
+        g_hGithub = CreateWindow(
+            L"STATIC",
+            L"GitHub 项目",
+            WS_VISIBLE | WS_CHILD | SS_NOTIFY,
+            20, 130, 120, 20,
+            hWnd,
+            (HMENU)3001,
+            NULL,
+            NULL);
+
+        // 设置下划线字体
+        LOGFONT lf;
+        GetObject(hFont, sizeof(LOGFONT), &lf);
+        lf.lfUnderline = TRUE;
+
+        g_hLinkFont = CreateFontIndirect(&lf);
+
+        SendMessage(g_hGithub, WM_SETFONT, (WPARAM)g_hLinkFont, TRUE);
+    }
+    break;
+
+    case WM_COMMAND:
+    {
+        if (LOWORD(wParam) == 3001 && HIWORD(wParam) == STN_CLICKED)
+        {
+            ShellExecute(
+                NULL,
+                L"open",
+                L"https://github.com/yangjule126/RestMinder",
+                NULL,
+                NULL,
+                SW_SHOWNORMAL);
+        }
+    }
+    break;
+
+    case WM_CTLCOLORSTATIC:
+    {
+        HDC hdc = (HDC)wParam;
+        HWND hCtrl = (HWND)lParam;
+
+        if (hCtrl == g_hGithub)
+        {
+            SetTextColor(hdc, RGB(0, 102, 204));
+            SetBkMode(hdc, TRANSPARENT);
+            return (LRESULT)GetStockObject(NULL_BRUSH);
+        }
+    }
+    break;
+    case WM_SETCURSOR:
+    {
+        POINT pt;
+        GetCursorPos(&pt);
+        ScreenToClient(hWnd, &pt);
+
+        RECT rc;
+        GetWindowRect(g_hGithub, &rc);
+        MapWindowPoints(HWND_DESKTOP, hWnd, (LPPOINT)&rc, 2);
+
+        if (PtInRect(&rc, pt))
+        {
+            SetCursor(LoadCursor(NULL, IDC_HAND));  // 手型
+            return TRUE;
+        }
+        else
+        {
+            SetCursor(LoadCursor(NULL, IDC_ARROW)); // 恢复箭头
+            return TRUE;
+        }
+    }
+    break;
+
+    case WM_CLOSE:
+        DestroyWindow(hWnd);
+        break;
     }
 
     return DefWindowProc(hWnd, msg, wParam, lParam);
@@ -242,7 +465,7 @@ LRESULT CALLBACK MainProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
         case ID_TRAY_STATUS:
         {
             wchar_t buffer[128];
-            swprintf_s(buffer, L"当前工作时间: %llu 分钟", workingTime / 60000);
+            swprintf_s(buffer, L"已连续使用电脑时间: %llu 分钟", workingTime / 60000);
             MessageBox(NULL, buffer, L"状态", MB_OK);
             break;
         }
@@ -254,7 +477,7 @@ LRESULT CALLBACK MainProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
                     L"SettingsWindow",
                     L"设置",
                     WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU,
-                    600, 300, 300, 250,
+                    600, 300, 390, 250,
                     NULL, NULL,
                     GetModuleHandle(NULL),
                     NULL);
@@ -310,6 +533,12 @@ int WINAPI WinMain(HINSTANCE hInstance,
     wc2.hInstance = hInstance;
     wc2.lpszClassName = L"SettingsWindow";
     RegisterClass(&wc2);
+
+    WNDCLASS wcAbout = { 0 };
+    wcAbout.lpfnWndProc = AboutProc;
+    wcAbout.hInstance = hInstance;
+    wcAbout.lpszClassName = L"AboutWindow";
+    RegisterClass(&wcAbout);
 
     HWND hWnd = CreateWindow(L"MainWindow", L"", WS_OVERLAPPEDWINDOW,
         0, 0, 0, 0,
